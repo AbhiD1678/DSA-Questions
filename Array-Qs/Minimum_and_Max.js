4f7a5d0157377c6978f98b8a700d3ab8e08c0802
Abhishek Dubey <avdubey_b21@el.vjti.ac.in>
2023-09-22 18:01:24 +0530

    adding optimal solution

--- Array-Qs/Minimum_and_Max.js
+++ Array-Qs/Minimum_and_Max.js
@@ -26,3 +26,67 @@ var findMaxMin = function(arr){
     return minmax
 }
 
+/*Second approach 
+Using Linear Search
+*/
+var MinMax=function(arr){
+    let minmax={}
+    minmax.min=arr[0]
+    minmax.max=arr[0]
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]<minmax.min){
+            minmax.min=arr[i]
+        }
+        else if(arr[i]>minmax.max){
+            minmax.max=arr[i]
+
+        }
+    }
+    return minmax
+}
+
+/*Third Approach,the best one with time complexity of o(n) */
+var MinMax=function(arr){
+let n = arr.length
+let mx,mn,i
+ 
+// If array has even number of elements then
+// initialize the first two elements as minimum
+// and maximum
+if(n % 2 == 0){
+    mx = Math.max(arr[0], arr[1])
+    mn = Math.min(arr[0], arr[1])
+     
+    // set the starting index for loop
+    i = 2
+}
+     
+// If array has odd number of elements then
+// initialize the first element as minimum
+// and maximum
+else{
+    mx = mn = arr[0]
+     
+    // set the starting index for loop
+    i = 1
+}
+     
+// In the while loop, pick elements in pair and
+// compare the pair with max and min so far
+while(i < n - 1){
+    if(arr[i] < arr[i + 1]){
+        mx = Math.max(mx, arr[i + 1])
+        mn = Math.min(mn, arr[i])
+    }
+    else{
+        mx = Math.max(mx, arr[i])
+        mn = Math.min(mn, arr[i + 1])
+    }
+         
+    // Increment the index by 2 as two
+    // elements are processed in loop
+    i += 2
+}
+ 
+return [mx, mn]
+}
