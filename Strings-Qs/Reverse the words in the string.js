0a702d85aee7f40f5dbea4731873ac4133631d83
Abhishek Dubey <avdubey_b21@el.vjti.ac.in>
2023-09-28 13:28:43 +0530

    adding solution in js

---
+++ Strings-Qs/Reverse the words in the string.js
@@ -0,0 +1,13 @@
+/*
+Same question but solved in js
+*/
+
+class Solution 
+{
+    //Function to reverse words in a given string.
+    reverseWords(s)
+    {
+        return s.split('.').reverse().join('.')
+        // code here
+    }
+}
