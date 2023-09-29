b484d31a1ccded8b86d58f55b2fbf276fee0d7a8
Abhishek Dubey <avdubey_b21@el.vjti.ac.in>
2023-09-29 11:32:53 +0530

    reverse an array or string

---
+++ Array-Qs/Reverse_an_array.js
@@ -0,0 +1,26 @@
+/* 
+Given an array (or string), the task is to reverse the array/string.
+Examples : 
+ 
+
+Input  : arr[] = {1, 2, 3}
+Output : arr[] = {3, 2, 1}
+
+Input :  arr[] = {4, 5, 1, 2}
+Output : arr[] = {2, 1, 5, 4}
+
+*/
+
+/* Normal Methods*/
+
+// M1 ,these are using in built function
+function ReverseArray(arr){
+    let arr1=arr
+    return arr1.reverse()
+}
+
+//M2 
+
+const ReverseArray1=(arr)=>{
+    return arr.reverse()
+}
