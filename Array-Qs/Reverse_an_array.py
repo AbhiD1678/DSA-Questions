99b8eff9c2532abbff5969e7eaeb01794fbc6e80
Abhishek Dubey <avdubey_b21@el.vjti.ac.in>
2023-09-29 11:36:53 +0530

    reverse an array or string in python

---
+++ Array-Qs/Reverse_an_array.py
@@ -0,0 +1,20 @@
+'''
+
+Given an array (or string), the task is to reverse the array/string.
+Examples : 
+ 
+
+Input  : arr[] = {1, 2, 3}
+Output : arr[] = {3, 2, 1}
+
+Input :  arr[] = {4, 5, 1, 2}
+Output : arr[] = {2, 1, 5, 4}
+
+
+'''
+
+# In built methods
+
+def reversearray(arr):
+    return arr.reverse()
+
