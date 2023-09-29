9ee2edb9dae5677705f9b962f52fb0fdb9d82783
Abhishek Dubey <avdubey_b21@el.vjti.ac.in>
2023-09-29 12:08:16 +0530

    reverse an array or string in python with optimised solutions 3 methods in javascript

--- Array-Qs/Reverse_an_array.js
+++ Array-Qs/Reverse_an_array.js
@@ -19,8 +19,26 @@ function ReverseArray(arr){
     return arr1.reverse()
 }
 
-//M2 
+//M2 using swappin
 
-const ReverseArray1=(arr)=>{
-    return arr.reverse()
+const ReverseArray1=(arr,start,end)=>{
+    while(start<end){
+        let temp=arr[start]
+        arr[start]=arr[end]
+        arr[end=temp]
+    
+    }
+    return arr
+}
+
+//M3 using recursion
+
+function ReverseArray2(arr,start=0,end=arr.length -1 ){
+    if(start<=end){
+        let temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+
+    };
+    return arr;
 }
