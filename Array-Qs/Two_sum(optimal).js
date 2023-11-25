bc98866a19f310f44480a8cb1940e4240acf6f9d
AbhiD1678 <104382962+AbhiD1678@users.noreply.github.com>
2023-11-25 14:34:49 +0530

    Update Two_sum(optimal).js

--- Array-Qs/Two_sum(optimal).js
+++ Array-Qs/Two_sum(optimal).js
@@ -51,3 +51,32 @@ var two_sum=function TwoSum(nums,target){
     return []
 
 }
+/*
+Dry run for first method
+numbertomap={} when 2 is given then 
+numbertomap[2] will be checked and its not available in numbertomap
+7 is appended in the numbertomap,so numbertomap={7:0}
+then when 7 is taken as the input,it is present in numbertomap so we get the output
+
+*/
+
+// Method 2
+var TwoSum=function TwoSum(nums,target){
+ let numbertomap={}
+ for(let p =0;p<nums.length;p++){
+  let target_value=target-nums[p]
+  let constNumber=numbertomap[target_value]
+  if(constNumber>=0){
+    return [p,constNumber]
+  else{
+       numbertomap[nums[p]]=p
+  }
+
+  /*
+  Dry run for first test case
+  numbertomap={} when 2 is the input
+  then numbertomap[7] is seen if its available or not,it is not for 2
+  then numbertomap={2:0}
+  when 7 is taken as the input,numbertomap[2] is present and we get the output
+  
+  */
